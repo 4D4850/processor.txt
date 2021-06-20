8e2488c61342edf33fc995a4c17525b45daaba1b
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2021-06-20 03:53:19 -0400

    Update processor.txt.py

--- processor.txt.py
+++ processor.txt.py
@@ -1,119 +1,112 @@
 import sys
 
-read = open("code.pt", "r")
+
+def dump(mode, msg=''):
+  if mode == 'debug':
+    print('Performing Debug Memory Dump')
+  else:
+    print(f'{msg}. Performing Memory Dump.')
+  write = open(f'{mode}dump.pt.txt', "w")
+  write.write(str(data))
+  write.close()
+  sys.exit()
+
+
 if '-c' in sys.argv:
   codeint = sys.argv.index("-c") + 1
   code = sys.argv[codeint]
 else:
+  read = open("code.pt", "r")
   code = read.readline()
-read.close()
+  read.close()
 
 print(type(code))
 
 dataPointer = 0
 codePointer = 0
-data = []
 selected_reg = 8
-reg = [0, 0, 0, 0, 0, 0, 0, 0]
-for i in range(1000):
-  data.append(0)
+data = [0 for _ in range(1000)]
+reg = [0 _ in range(8)]
+
 while codePointer < len(code):
   # This is the main interpret loop
-  if code[codePointer] == '!':
+  if (char := code[codePointer]) == '!':
     data[dataPointer] = 255 - data[dataPointer]
-  elif code[codePointer] == '&':
+  elif char == '&':
     if selected_reg == 8:
-      print("Error: No Register Selected. Performing Memory Dump.")
-      write = open("memdump.pt.txt", "w")
-      write.write(str(data))
-      write.close()
-      sys.exit()
+      dump('mem', 'Error: No Register Selected')
     data[dataPointer] = reg[selected_reg] & data[dataPointer]
-  elif code[codePointer] == '^':
+  elif char == '^':
     if selected_reg == 8:
-      print("Error: No Register Selected. Performing Memory Dump.")
-      write = open("memdump.pt.txt", "w")
-      write.write(str(data))
-      write.close()
-      sys.exit()
+      dump('mem', 'Error: No Register Selected')
     data[dataPointer] = reg[selected_reg] ^ data[dataPointer]
-  elif code[codePointer] == ',':
+  elif char == ',':
     reg[0] = data[dataPointer]
     selected_reg = 0
-  elif code[codePointer] == '$':
+  elif char == '$':
     data[dataPointer] = reg[0]
-  elif code[codePointer] == '.':
+  elif char == '.':
     reg[1] = data[dataPointer]
     selected_reg = 1
-  elif code[codePointer] == '*':
+  elif char == '*':
     data[dataPointer] = reg[1]
     reg[1] = 0
-  elif code[codePointer] == '/':
+  elif char == '/':
     reg[2] = data[dataPointer]
     selected_reg = 2
-  elif code[codePointer] == '?':
+  elif char == '?':
     data[dataPointer] = reg[2]
     reg[2] = 0
-  elif code[codePointer] == ';':
+  elif char == ';':
     reg[3] = data[dataPointer]
     selected_reg = 3
-  elif code[codePointer] == ':':
+  elif char == ':':
     data[dataPointer] = reg[3]
     reg[3] = 0
-  elif code[codePointer] == '@':
+  elif char == '@':
     reg[4] = data[dataPointer]
     selected_reg = 4
-  elif code[codePointer] == '#':
+  elif char == '#':
     data[dataPointer] = reg[4]
     reg[4] = 0
-  elif code[codePointer] == '[':
+  elif char == '[':
     reg[5] = data[dataPointer]
     selected_reg = 5
-  elif code[codePointer] == '{':
+  elif char == '{':
     data[dataPointer] = reg[5]
     reg[5] = 0
-  elif code[codePointer] == ']':
+  elif char == ']':
     reg[6] = data[dataPointer]
     selected_reg = 6
-  elif code[codePointer] == '}':
+  elif char == '}':
     data[dataPointer] = reg[6]
     reg[6] = 0
-  elif code[codePointer] == '\\':
+  elif char == '\\':
     reg[7] = data[dataPointer]
     selected_reg = 7
-  elif code[codePointer] == '|':
+  elif char == '|':
     data[dataPointer] = reg[7]
     reg[7] = 0
-  elif code[codePointer] == '>':
+  elif char == '>':
     dataPointer += 1
-  elif code[codePointer] == '<':
+  elif char == '<':
     dataPointer -= 1
-  elif code[codePointer] == '+':
-    data[dataPointer] = (data[dataPointer] + 1)%256
-  elif code[codePointer] == '-':
-    data[dataPointer] = (data[dataPointer] - 1)%256
-  elif code[codePointer] == '~':
+  elif char == '+':
+    data[dataPointer] = (data[dataPointer] + 1) % 256
+  elif char == '-':
+    data[dataPointer] = (data[dataPointer] - 1) % 256
+  elif char == '~':
     if '-a' in sys.argv:
       print(chr(data[dataPointer]))
     else:
       print(data[dataPointer])
-  elif code[codePointer] == '%':
+  elif char == '%':
     data[dataPointer] = data[dataPointer] << 1
-  elif code[codePointer] == '_':
+  elif char == '_':
     data[dataPointer] = data[dataPointer] >> 1
-  elif code[codePointer] == '`':
-    pass
-  else:
-    print(f"{code[codePointer]} is not a valid command. Performing Memory Dump.")
-    write = open("memdump.pt.txt", "w")
-    write.write(str(data))
-    write.close()
-    sys.exit()
+  elif char != '`':
+    dump('mem', f'{code[codePointer]} is not a valid command')
   codePointer += 1
 
 if '-d' in sys.argv  or '--debug' in sys.argv:
-  print("Performing Debug Memory Dump")
-  write = open("debugdump.pt.txt", "w")
-  write.write(str(data))
-  write.close()
-sys.exit()
+  dump('debug')
